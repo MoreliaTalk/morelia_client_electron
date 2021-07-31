c03ddf38c21791907c1fe9808df9f1ec445d7919
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2021-07-31 20:16:57 +0500

    add style itextarea and button

--- src/render/source/components/InputPole.tsx
+++ src/render/source/components/InputPole.tsx
@@ -23,33 +23,11 @@ class InputPole extends Component {
         this.props.send_method("other_user", ((document.getElementById("text_input") as HTMLInputElement).value))
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            100
-        );
-    }
-    
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-    
-    tick() {
-        if (Number(document.getElementById("text_input").style.height.slice(0, -2)) != this.state.add_height){
-            this.setState({
-            add_height: Number(document.getElementById("text_input").style.height.slice(0, -2))
-            });
-            document.getElementById("root").style.gridTemplateRows = "1fr "+ String(57+this.state.add_height)+"px"
-        }
-    }
-
     render() {
         //TODO Заменить простые компоненты на более эстетичные
         return (
                 <div id="forma_input">
-                    <div id="text_input_container">
-                        <textarea id="text_input" placeholder = "Введите текст сообщения:"/>
-                    </div>
+                    <textarea id="text_input" placeholder = "Введите текст сообщения:"/>
                     <button id="button_send" onClick={this.send}>Отправить</button>
                 </div>
         );
